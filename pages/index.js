85c93b05d4692364adbd99b3c87f96ae73c77cd7
peterlidee <peter@lidee.be>
2023-01-19 14:52:56 +0100

    chap20

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,1 @@
-export default function IndexPage() {
-  return <div>IndexPage</div>;
-}
+export { default } from './products';
--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,7 @@
+import Products from '../components/Products';
+
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return <Products />;
 }
 
 export default ProductsPage;
