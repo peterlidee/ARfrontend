4880b2bf930ee456ced9ccd495cd9dd195b9bd75
peterlidee <peter@lidee.be>
2023-01-27 18:36:27 +0100

    chap 63

--- pages/orders.js
+++ pages/orders.js
@@ -1,5 +1,89 @@
-function OrderPage() {
-  return <div>OrderPage</div>;
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
+import Link from 'next/link';
+import Error from '../components/ErrorMessage';
+import OrderItemStyles from '../components/styles/OrderItemStyles';
+import formatMoney from '../lib/formatMoney';
+
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    allOrders {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+const OrderUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 4rem;
+`;
+
+function countItemsInAnOrder(order) {
+  return order.items.reduce((tally, item) => tally + item.quantity, 0);
 }
 
-export default OrderPage;
+function OrdersPage() {
+  const { loading, error, data } = useQuery(USER_ORDERS_QUERY);
+  if (loading) return '...loading';
+  if (error) {
+    return <Error error={error} />;
+  }
+  const { allOrders } = data;
+  return (
+    <div>
+      <Head>
+        <title>Your orders ({allOrders.length})</title>
+      </Head>
+      <h2>You have {allOrders.length}</h2>
+      <OrderUl>
+        {allOrders.map((order) => (
+          <OrderItemStyles>
+            <Link href={`order/${order.id}`}>
+              <a>
+                <div className="order-meta">
+                  <p>{countItemsInAnOrder(order)} Items</p>
+                  <p>
+                    {order.items.length} Product
+                    {order.items.length === 1 ? '' : 's'}
+                  </p>
+                  <p>{formatMoney(order.total)}</p>
+                </div>
+                <div className="images">
+                  {order.items.map((item) => (
+                    <img
+                      key={`image-${item.id}`}
+                      src={item?.photo?.image?.publicUrlTransformed}
+                      alt={item.name}
+                    />
+                  ))}
+                </div>
+              </a>
+            </Link>
+          </OrderItemStyles>
+        ))}
+      </OrderUl>
+    </div>
+  );
+}
+
+export default OrdersPage;
